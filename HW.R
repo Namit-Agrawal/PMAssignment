6975ad8a9141c483f90ec99183f37c66181575ce
timcheng29 <timothycheng29@gmail.com>
2020-08-12 14:53:40 -0500

    8/12/2020

--- HW.R
+++ HW.R
@@ -68,3 +68,88 @@ ggplot(data = sub,aes(x = Days,y=prop)) + geom_bar(stat = "identity",fill = 1:7)
 
 
 new <- subset[subset$Cancelled == 1,]
+
+
+####################################################
+
+library(quantmod)
+mystocks <- c("GLD","USO","VNQ","BNO","SLV")
+getSymbols(mystocks,from = "2014-01-01")
+
+###
+
+GLDa <- adjustOHLC(GLD)
+USOa <- adjustOHLC(USO)
+VNQa <- adjustOHLC(VNQ)
+BNOa <- adjustOHLC(BNO)
+SLVa <- adjustOHLC(SLV)
+
+###
+
+returns <- cbind(ClCl(GLDa),ClCl(USOa),ClCl(VNQa),ClCl(BNOa),ClCl(SLVa))
+all.r <- as.matrix(na.omit(returns))
+n <- nrow(all.r)
+
+#wealth <- c()
+boot.portfolio1 <- c()
+set.seed(1)
+for (i in 1:5000){
+  total <- 100000
+  weights <- rep(0.2,5)
+  holdings <- total * weights
+  for (i in 1:20){
+    return.day <- resample(all.r,1,orig.ids = FALSE)
+    holdings <- holdings*(1+return.day)
+    #wealth <- c(wealth,holdings)
+  }
+  boot.portfolio1 <- c(boot.portfolio1,sum(holdings))
+}
+
+#wealth <- c()
+boot.portfolio2 <- c()
+set.seed(1)
+for (i in 1:5000){
+  total <- 100000
+  weights <- c(0.96,0.01,0.01,0.01,0.01)
+  holdings <- total * weights
+  for (i in 1:20){
+    return.day <- resample(all.r,1,orig.ids = FALSE)
+    holdings <- holdings*(1+return.day)
+    #wealth <- c(wealth,holdings)
+  }
+  boot.portfolio2 <- c(boot.portfolio2,sum(holdings))
+}
+
+#wealth <- c()
+boot.portfolio3 <- c()
+set.seed(1)
+for (i in 1:5000){
+  total <- 100000
+  weights <- c(0.1,0.1,0.6,0.1,0.1)
+  holdings <- total * weights
+  for (i in 1:20){
+    return.day <- resample(all.r,1,orig.ids = FALSE)
+    holdings <- holdings*(1+return.day)
+    #wealth <- c(wealth,holdings)
+  }
+  boot.portfolio3 <- c(boot.portfolio3,sum(holdings))
+}
+
+hist(boot.portfolio1)
+hist(boot.portfolio2)
+hist(boot.portfolio3)
+
+summary(boot.portfolio1)
+summary(boot.portfolio2)
+summary(boot.portfolio3)
+
+abs(quantile(boot.portfolio1 - 100000,prob = 0.05))
+abs(quantile(boot.portfolio2 - 100000,prob = 0.05))
+abs(quantile(boot.portfolio3 - 100000,prob = 0.05))
+
+
+
+
+
+
+
