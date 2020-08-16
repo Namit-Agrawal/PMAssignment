91d061ac9c45ca529f581ae6115fb62af0ff6975
timcheng29 <timothycheng29@gmail.com>
2020-08-16 17:37:02 -0500

    Aug 16

--- HW.R
+++ HW.R
@@ -169,6 +169,256 @@ abs(quantile(boot.portfolio2 - 100000,prob = 0.05))
 abs(quantile(boot.portfolio3 - 100000,prob = 0.05))
 
 
+####################################################################
+
+
+social <- read.csv("social_marketing.csv",header = T)
+
+
+library(ggplot2)
+library(tidyverse)
+
+scaled <- as.data.frame(scale(social[,-c(1)],center = T,scale = T))
+mu <- attr(scale(social[,-c(1)]), "scaled:center")
+sigma <- attr(scale(social[,-c(1)]),"scaled:scale")
+
+
+#####
+ID <- as.data.frame(social[,1])
+
+pca.social <- prcomp(scaled)
+plot(pca.social)
+plot(summary(pca.social)$importance[3,])
+
+scaled.pca <- pca.social$x[,1:15]
+head(round(pca.social$rotation[,1:3],2))
+
+
+loadings.summary <- pca.social$rotation %>%
+  as.data.frame() %>%
+  rownames_to_column("Category")
+# spam
+spam <- loadings.summary %>%
+  select(Category,PC1) %>%
+  arrange(desc(PC1))
+
+# young
+young <- loadings.summary %>%
+  select(Category,PC2) %>%
+  arrange(desc(PC2))
+
+# fit
+fit <- loadings.summary %>%
+  select(Category,PC3) %>%
+  arrange(desc(PC3))
+
+# college gamer
+game <- loadings.summary %>%
+  select(Category,PC4) %>%
+  arrange(desc(PC4))
+
+# college gamer
+loadings.summary %>%
+  select(Category,PC5) %>%
+  arrange(desc(PC5))
+
+# blogger
+loadings.summary %>%
+  select(Category,PC6) %>%
+  arrange(desc(PC6))
+
+# artist/travel enthusiast
+art <- loadings.summary %>%
+  select(Category,PC7) %>%
+  arrange(desc(PC7))
+
+# singles
+loadings.summary %>%
+  select(Category,PC10) %>%
+  arrange(desc(PC10))
+
+
+
+followers <- as.data.frame(pca.social$x)
+rownames(followers) <- ID$social[, 1]
+
+ggplot(young, aes(x=reorder(Category,PC2), y=PC2)) +
+  geom_bar(stat='identity') + coord_flip()
+
+ggplot(fit, aes(x=reorder(Category,PC3), y=PC3)) +
+  geom_bar(stat='identity') + coord_flip()
+
+ggplot(game, aes(x=reorder(Category,PC4), y=PC4)) +
+  geom_bar(stat='identity') + coord_flip()
+
+
+
+scaled <- as.data.frame(scale(social[,-c(1)],center = T,scale = T))
+mu <- attr(scale(social[,-c(1)]), "scaled:center")
+sigma <- attr(scale(social[,-c(1)]),"scaled:scale")
+
+
+clust <- kmeanspp(scaled,k = 5,nstart = 25)
+colors <- clust$cluster
+
+subset <- pca.social$x[,c(3,4)]
+plot(subset, col = colors, pch = 16)
+x <- c("Fitness Enthusiast","College Student")
+cols <- c("black","turquiose")
+
+###################################################
+
+
+# ReadPlain function 
+
+setwd("C:/Users/timot/Documents/GitHub/PMAssignment/")
+library(tidyverse)
+library(tm)
+library(slam)
+library(proxy)
+
+readerPlain = function(fname){
+  readPlain(elem=list(content=readLines(fname)), 
+            id=fname, language='en') }
+
+
+file_list <- Sys.glob("ReutersC50/C50train/*")
+
+total <- c()
+author <- c()
+
+for (i in 1:length(file_list)){
+  articles <- Sys.glob(as.character(paste(file_list[i],"/*.txt", sep = "")))
+  author <- c(author,rep(strsplit(file_list[i],"/")[[1]][3],length(articles)))
+  total <- c(total,articles)
+}
+
+data <- cbind(author,total)
+data <- cbind(data,lapply(data[,2],readerPlain))
+
+
+documents_raw <- Corpus(VectorSource(data[,3]))
+
+my_documents <- documents_raw %>%
+  tm_map(content_transformer(tolower))  %>%             
+  tm_map(content_transformer(removeNumbers)) %>%        
+  tm_map(content_transformer(removePunctuation)) %>%    
+  tm_map(content_transformer(stripWhitespace))          
+
+my_documents <- tm_map(my_documents, content_transformer(removeWords), stopwords("en"))
+
+DTM_train <- DocumentTermMatrix(my_documents)
+inspect(DTM_train[1:10,1:20])
+
+DTM_train <- removeSparseTerms(DTM_train, 0.95)
+tfidf_train = weightTfIdf(DTM_train)
+
+# TEST SET
+
+file_list2 <- Sys.glob("ReutersC50/C50test/*")
+
+total2 <- c()
+author2 <- c()
+
+for (i in 1:length(file_list2)){
+  articles2 <- Sys.glob(as.character(paste(file_list2[i],"/*.txt", sep = "")))
+  author2 <- c(author2,rep(strsplit(file_list2[i],"/")[[1]][3],length(articles2)))
+  total2 <- c(total2,articles2)
+}
+
+data2 <- cbind(author2,total2)
+data2 <- cbind(data2,lapply(data2[,2],readerPlain))
+
+
+documents_raw2 <- Corpus(VectorSource(data2[,3]))
+
+my_documents2 <- documents_raw2 %>%
+  tm_map(content_transformer(tolower))  %>%             
+  tm_map(content_transformer(removeNumbers)) %>%        
+  tm_map(content_transformer(removePunctuation)) %>%    
+  tm_map(content_transformer(stripWhitespace))          
+
+my_documents2 <- tm_map(my_documents2, content_transformer(removeWords), stopwords("en"))
+
+DTM_test <- DocumentTermMatrix(my_documents2)
+inspect(DTM_test[1:10,1:20])
+
+DTM_test <- removeSparseTerms(DTM_test, 0.95)
+tfidf_test <- weightTfIdf(DTM_test)
+
+# FILTER
+
+X_train <- as.matrix(tfidf_train)
+scrub <- which(colSums(X_train) == 0)
+X_train <- X_train[,-scrub]
+
+X_test <- as.matrix(tfidf_test)
+scrub2 <- which(colSums(X_test) == 0)
+X_test <- X_test[,-scrub2]
+
+
+# MATCH
+
+train.cols <- colnames(X_train)
+test.cols <- colnames(X_test)
+
+match <- intersect(train.cols,test.cols)
+X_test <- X_test[,match]
+X_train <- X_train[,match]
+
+
+# DIMENSION REDUCTION
+
+pca.x <- prcomp(X_train,scale=T)
+plot(summary(pca.x)$importance[3,])
+
+train <- pca.x$x[,1:400]
+test <- predict(pca.x,newdata = X_test)[,1:400]
+
+# PREDICTION
+
+train <- as.data.frame(cbind(author,train))
+test <- as.data.frame(cbind(author2,test))
+colnames(test)[length(test)] <- "author"
+
+library(randomForest)
+
+set.seed(1)
+rf <- randomForest(as.factor(author)~.,data = train,importance = T,mtry = 5,ntree =100)
+
+preds <- predict(rf,newdata = test)
+tab <- table(preds,as.factor(test$author))
+totalsum <- 0
+for (i in 1:dim(tab)[1]){
+  totalsum <- totalsum + tab[i,i]
+}
+
+
+
+# KNN
+
+library(kknn)
+
+tr <- train
+t <- test
+
+knn <- kknn(as.factor(author)~.,train = tr, test = t, k = 50)
+
+
+
+
+# Boosting
+
+#library(gbm)
+
+#boosted <- gbm(as.factor(author)~.,data = train)
+
+
+
+
+
+
+
 
 
 
